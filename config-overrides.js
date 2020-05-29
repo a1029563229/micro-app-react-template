f7f9f91eea4bcec1105499f8d26ddad1f31e19aa
lt <1029563229@qq.com>
2020-05-29 16:19:09 +0800

    🎉 初始化 React 微前端模板

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  webpack: (config) => {
+    return config;
+  },
+
+  devServer: function (configFunction) {
+    return function (proxy, allowedHost) {
+      return config;
+    };
+  },
+};
